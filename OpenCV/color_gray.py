2a55700b37d62f432a450ab0f6cbdd3f284aa632
Ashil-James <ashilgjames@gmail.com>
2026-03-16 06:56:14 +0530

    feat: started openc

---
+++ OpenCV/color_gray.py
@@ -0,0 +1,21 @@
+import cv2
+
+# Load the image
+image = cv2.imread('cyber_logopng.png')
+
+# color to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('gray_image.jpg', gray)
+
+# grayscale to color
+gray_bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+cv2.imwrite('gray_to_color.jpg', gray_bgr)
+
+# With false color (heatmap style)
+heatmap = cv2.applyColorMap(gray, cv2.COLORMAP_JET)
+cv2.imshow('False Color', heatmap)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
