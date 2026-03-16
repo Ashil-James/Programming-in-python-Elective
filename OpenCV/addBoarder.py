139d6b7069552420f4fb56e5f221f02838badbbd
Ashil-James <ashilgjames@gmail.com>
2026-03-16 21:49:49 +0530

    feat: adding border to image

---
+++ OpenCV/addBoarder.py
@@ -0,0 +1,18 @@
+import cv2
+
+img = cv2.imread('cyber_logopng.png')
+# Add border to the image
+
+border = cv2.copyMakeBorder(
+    img,
+    top = 50,
+    bottom = 50,
+    left = 50,
+    right = 50,
+    borderType = cv2.BORDER_CONSTANT,
+    value = [0, 0, 255]
+)
+
+cv2.imshow('Bordered Image', border)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
